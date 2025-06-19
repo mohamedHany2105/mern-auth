5ead56dec20d90f8a39c9e6425301ae9e589e333
mohamedHany2105 <mohamed2105hany@gmail.com>
2025-06-19 11:07:10 +0300

    slsl

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div className='text-red-400 '>Al 3awda al montazera</div>
+    <div className='text-red-400 '>
+      Al 3awda al montazera
+      Al 3awda al montazera
+      Al 3awda al montazera
+      Al 3awda al montazera
+      Al 3awda al montazera
+      Al 3awda al montazera
+      Al 3awda al montazera
+      Al 3awda al montazera
+      Al 3awda al montazera
+    </div>
   )
 } 
